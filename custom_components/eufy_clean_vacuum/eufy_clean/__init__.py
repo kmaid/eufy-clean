9895d81c39da6214f6cc2d03cf7626b9cea433c5
Thomas Waterfall <thomas.waterfall@gmail.com>
2024-12-30 20:14:49 +0000

    Comment out most of the project

--- custom_components/eufy_clean_vacuum/eufy_clean/__init__.py
+++ custom_components/eufy_clean_vacuum/eufy_clean/__init__.py
@@ -1,61 +1,21 @@
 """Eufy Clean API client."""
 import logging
 from typing import Dict, List, Optional
-import uuid
-
-from .controllers.login import EufyLogin
-from .controllers.mqtt_connect import MqttConnect
-from .controllers.local_connect import LocalConnect
-from .controllers.cloud_connect import CloudConnect
 
 _LOGGER = logging.getLogger(__name__)
-
 
 class EufyClean:
     """Eufy Clean API client."""
 
-    def __init__(self, username: str, password: str, openudid: Optional[str] = None):
-        """Initialize Eufy Clean API client.
-
-        Args:
-            username: Eufy account email
-            password: Eufy account password
-            openudid: Optional unique device identifier
-        """
-        if not openudid:
-            openudid = str(uuid.uuid4())
-
-        self.eufy_login = EufyLogin(username, password, openudid)
-        self.mqtt_connect: Optional[MqttConnect] = None
+    def __init__(self, username: str, password: str):
+        """Initialize the API client."""
+        self.username = username
+        self.password = password
 
     async def init(self) -> None:
-        """Initialize connection and fetch device lists."""
-        await self.eufy_login.init()
-
-    async def get_mqtt_devices(self) -> List[Dict]:
-        """Get list of MQTT-connected devices."""
-        return await self.eufy_login.get_device_list()
-
-    async def get_cloud_devices(self) -> List[Dict]:
-        """Get list of cloud-connected devices."""
-        return await self.eufy_login.get_cloud_device_list()
-
-    async def get_device_properties(self, device_model: str) -> None:
-        """Get device properties."""
-        return await self.eufy_login.get_device_properties(device_model)
-
-    async def get_mqtt_credentials(self) -> Dict:
-        """Get MQTT credentials."""
-        return await self.eufy_login.get_mqtt_credentials()
-
-    async def get_user_info(self) -> Dict:
-        """Get user information."""
-        return await self.eufy_login.get_user_info()
-
-    async def get_cloud_device(self, device_id: str) -> Optional[Dict]:
-        """Get cloud device by ID."""
-        return await self.eufy_login.get_cloud_device(device_id)
-
-    async def send_cloud_command(self, device_id: str, command: Dict) -> None:
-        """Send command to cloud device."""
-        await self.eufy_login.send_cloud_command(device_id, command)
+        """Test the credentials by attempting to initialize."""
+        # For now, just log the attempt. We'll add real auth later
+        _LOGGER.debug("Testing connection with username: %s", self.username)
+        # In a real implementation, this would try to authenticate
+        # For now, we'll just pretend it worked
+        return
